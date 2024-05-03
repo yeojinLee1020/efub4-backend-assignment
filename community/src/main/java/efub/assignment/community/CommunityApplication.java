e9cb936dcbe3b61524db2b36970b6a764a45e95c
이여진 <yeojinlee1020@gmail.com>
2024-05-03 11:42:04 +0900

    fix : 누락된 @EnableJpaAuditing 어노테이션 추가

--- community/src/main/java/efub/assignment/community/CommunityApplication.java
+++ community/src/main/java/efub/assignment/community/CommunityApplication.java
@@ -2,7 +2,9 @@ package efub.assignment.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 엔티티 생성,수정되는 그 시점을 감지하여 생성시가, 수정시각을 기록할 수 있음
 @SpringBootApplication
 public class CommunityApplication {
 
