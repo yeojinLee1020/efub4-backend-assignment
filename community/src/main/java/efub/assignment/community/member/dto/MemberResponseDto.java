8d291000895d129f1fe8dd9a1244f80dd3c7e270
이여진 <yeojinlee1020@gmail.com>
2024-05-03 11:56:55 +0900

    refactor : 모든 필드 가진 생성자를 @AllArgsConstructor로 교체

--- community/src/main/java/efub/assignment/community/member/dto/MemberResponseDto.java
+++ community/src/main/java/efub/assignment/community/member/dto/MemberResponseDto.java
@@ -2,25 +2,19 @@ package efub.assignment.community.member.dto;
 
 import efub.assignment.community.member.domain.Member;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class MemberResponseDto {
     private Long accountId;
     private String email;
     private String nickname;
     private String studentId;
     private String university;
-
-    public MemberResponseDto(Long accountId, String email, String nickname, String studentId, String university) {
-        this.accountId = accountId;
-        this.email = email;
-        this.nickname = nickname;
-        this.studentId = studentId;
-        this.university = university;
-    }
 
     public static MemberResponseDto from (Member member) {
         return new MemberResponseDto(member.getMemberId(),
